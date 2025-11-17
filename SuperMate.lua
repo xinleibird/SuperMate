6ef2abe94ad0f7831bd2e08174e2ed9f0ec3c677
辛磊 <xinleibird@gmail.com>
2025-11-18 03:58:44 +0800

    update

--- SuperMate.lua
+++ SuperMate.lua
@@ -1,8 +1,8 @@
-SuperMate = CreateFrame("Frame", nil, nil)  
+SuperMate = CreateFrame("Frame", nil, nil)
 
 SuperMate:RegisterEvent("UNIT_CASTEVENT")
 
-local SM = {};
+local SM = {}
 SM.spellID = 0
 SM.dura = 0
 SM.defaultDura = 2500
@@ -24,33 +24,34 @@ SuperMate:SetScript("OnEvent", function()
 end)
 
 function SM.UnitCastEvent(arg1, arg2, arg3, arg4, arg5)
-	local _,target_guid = UnitExists("target")
-	local _,player_guid = UnitExists("player")
+	local _, target_guid = UnitExists("target")
+	local _, player_guid = UnitExists("player")
 
 	if target_guid == arg2 then
-		if arg3=="START" or arg3 == "CHANNEL" then
+		if arg3 == "START" or arg3 == "CHANNEL" then
 			--DEFAULT_CHAT_FRAME:AddMessage("cast state:"..tostring(arg3).." -spellID:"..tostring(arg4).." -dura:"..tostring(arg5))
 			SM.spellID = arg4
 			SM.dura = arg5
 			SM.targetID = arg2
-		elseif arg3=="CAST" or arg3 == "FAIL" then
+		elseif arg3 == "CAST" or arg3 == "FAIL" then
 			SM.spellID = 0
 			SM.dura = 0
 			SM.targetID = 0
 		end
 	end
-	if player_guid == arg1 then 
-		if arg3=="START" or arg3 == "CHANNEL" then
+	if player_guid == arg1 then
+		if arg3 == "START" or arg3 == "CHANNEL" then
 			SM.selfCasting = 1
-		elseif  arg3=="CAST" or arg3 == "FAIL" then
+		elseif arg3 == "CAST" or arg3 == "FAIL" then
 			SM.selfCasting = 0
 		end
 	end
 end
 
 SuperMate.IsCastingIncludeName = function(SpellName)
-	b,i=UnitExists("target")
-	if not b or SM.spellID == 0 or SM.dura == 0 or i~= SM.targetID then
+	---@diagnostic disable-next-line: lowercase-global
+	b, i = UnitExists("target")
+	if not b or SM.spellID == 0 or SM.dura == 0 or i ~= SM.targetID then
 		return false
 	end
 	local sname = SpellInfo(SM.spellID)
@@ -64,8 +65,9 @@ SuperMate.IsCastingIncludeName = function(SpellName)
 end
 
 SuperMate.ifCastingTime = function(Dura)
-	b,i=UnitExists("target")
-	if not b or SM.spellID == 0 or SM.dura == 0 or i~= SM.targetID then
+	---@diagnostic disable-next-line: lowercase-global
+	b, i = UnitExists("target")
+	if not b or SM.spellID == 0 or SM.dura == 0 or i ~= SM.targetID then
 		return false
 	end
 	local d = Dura or SM.defaultDura
@@ -79,50 +81,59 @@ SuperMate.PlayerInCasting = function()
 	return SM.selfCasting
 end
 
-SuperMate.IsMoving = false;
-SuperMate.m_vCurrPos = {};
+SuperMate.IsMoving = false
+SuperMate.m_vCurrPos = {}
 
-SuperMate.m_vLastPos = {};
-SuperMate.m_vLastPos.x, SuperMate.m_vLastPos.y = GetPlayerMapPosition("player");
+SuperMate.m_vLastPos = {}
+SuperMate.m_vLastPos.x, SuperMate.m_vLastPos.y = GetPlayerMapPosition("player")
 
 SuperMate:SetScript("OnUpdate", function()
-	SuperMate.m_vCurrPos.x, SuperMate.m_vCurrPos.y = GetPlayerMapPosition("player");
-	SuperMate.m_vCurrPos.x = SuperMate.m_vCurrPos.x + 0.0;
-	SuperMate.m_vCurrPos.y = SuperMate.m_vCurrPos.y + 0.0;
-	
-	if (SuperMate.m_vCurrPos.x) then
-		local dist;
-		
-		-- travel speed ignores Z-distance (i.e. you run faster up or down hills)	
+	SuperMate.m_vCurrPos.x, SuperMate.m_vCurrPos.y = GetPlayerMapPosition("player")
+	SuperMate.m_vCurrPos.x = SuperMate.m_vCurrPos.x + 0.0
+	SuperMate.m_vCurrPos.y = SuperMate.m_vCurrPos.y + 0.0
+
+	if SuperMate.m_vCurrPos.x then
+		local dist
+
+		-- travel speed ignores Z-distance (i.e. you run faster up or down hills)
 		-- x and y coords are not square, had to weight the x by 2.25 to make the readings match the y axis.
 		dist = math.sqrt(
-				((SuperMate.m_vLastPos.x - SuperMate.m_vCurrPos.x) * (SuperMate.m_vLastPos.x - SuperMate.m_vCurrPos.x) * 2.25 ) +
-				((SuperMate.m_vLastPos.y - SuperMate.m_vCurrPos.y) * (SuperMate.m_vLastPos.y - SuperMate.m_vCurrPos.y)));
-		
-		if (dist > 0) then
-			SuperMate.IsMoving = true;		
+			(
+				(SuperMate.m_vLastPos.x - SuperMate.m_vCurrPos.x)
+				* (SuperMate.m_vLastPos.x - SuperMate.m_vCurrPos.x)
+				* 2.25
+			)
+				+ (
+					(SuperMate.m_vLastPos.y - SuperMate.m_vCurrPos.y)
+					* (SuperMate.m_vLastPos.y - SuperMate.m_vCurrPos.y)
+				)
+		)
+
+		if dist > 0 then
+			SuperMate.IsMoving = true
 		else
-			SuperMate.IsMoving = false;
+			SuperMate.IsMoving = false
 		end
 
-		SuperMate.m_vLastPos.x = SuperMate.m_vCurrPos.x;
-		SuperMate.m_vLastPos.y = SuperMate.m_vCurrPos.y;
-		SuperMate.m_vLastPos.z = SuperMate.m_vCurrPos.z;
+		SuperMate.m_vLastPos.x = SuperMate.m_vCurrPos.x
+		SuperMate.m_vLastPos.y = SuperMate.m_vCurrPos.y
+		SuperMate.m_vLastPos.z = SuperMate.m_vCurrPos.z
 	end
 end)
 
 local function MonkeySpeed_Round(x)
-	if(x - floor(x) > 0.5) then
-		x = x + 0.5;
+	if x - floor(x) > 0.5 then
+		x = x + 0.5
 	end
-	return floor(x);
+	return floor(x)
 end
 
 function SM.IdAndNameFromLink(link)
 	local name
-	if (not link) then
+	if not link then
 		return ""
 	end
+	---@diagnostic disable-next-line: undefined-field
 	for id, name in string.gfind(link, "|c%x+|Hitem:(%d+):%d+:%d+:%d+|h%[(.-)%]|h|r$") do
 		return tonumber(id), name
 	end
@@ -132,19 +143,19 @@ end
 SuperMate.IsEquipped = function(item)
 	local itemId
 	local itemName
-	if (string.find(item, '^%d')) then
+	if string.find(item, "^%d") then
 		itemId = tonumber(item)
 	else
 		itemName = item
 	end
 	for slot = 0, 19 do
 		local link = GetInventoryItemLink("player", slot)
-		if (link) then
+		if link then
 			local id, name = SM.IdAndNameFromLink(link)
-			if (id) then
-				if ((itemId and id == itemId)) then
+			if id then
+				if itemId and id == itemId then
 					return true
-				elseif (itemName) then
+				elseif itemName then
 					if string.lower(name) == string.lower(itemName) then
 						return true
 					end
@@ -156,74 +167,76 @@ SuperMate.IsEquipped = function(item)
 end
 
 SuperMate.GetSpellCooldownByName = function(spellName)
-    local checkFor = function(bookType)
-        local i = 1
-        while true do
-            local name, spellRank = GetSpellName(i, bookType);
-            
-            if not name then
-                break;
-            end
-            
-            if name == spellName then
-                local _, duration = GetSpellCooldown(i, bookType);
-                return duration;
-            end
-            
-            i = i + 1
-        end
-        return nil;
-    end
-    
-    
-    local cd = checkFor(BOOKTYPE_PET);
-    if not cd then cd = checkFor(BOOKTYPE_SPELL); end
-    
-    return cd;
+	local checkFor = function(bookType)
+		local i = 1
+		while true do
+			local name, spellRank = GetSpellName(i, bookType)
+
+			if not name then
+				break
+			end
+
+			if name == spellName then
+				local _, duration = GetSpellCooldown(i, bookType)
+				return duration
+			end
+
+			i = i + 1
+		end
+		return nil
+	end
+
+	local cd = checkFor(BOOKTYPE_PET)
+	if not cd then
+		cd = checkFor(BOOKTYPE_SPELL)
+	end
+
+	return cd
 end
 
 SuperMate.GetSpellTextureByName = function(spellName)
-    local checkFor = function(bookType)
-        local i = 1
-        while true do
-            local name, spellRank = GetSpellName(i, bookType);
-            
-            if not name then
-                break;
-            end
-            
-            if name == spellName then
-                local icon = GetSpellTexture(i, bookType);
-                return icon;
-            end
-            
-            i = i + 1
-        end
-        return nil;
-    end
-    
-    
-    local texture = checkFor(BOOKTYPE_PET);
-    if not texture then texture = checkFor(BOOKTYPE_SPELL); end
-    
-    return texture;
+	local checkFor = function(bookType)
+		local i = 1
+		while true do
+			local name, spellRank = GetSpellName(i, bookType)
+
+			if not name then
+				break
+			end
+
+			if name == spellName then
+				local icon = GetSpellTexture(i, bookType)
+				return icon
+			end
+
+			i = i + 1
+		end
+		return nil
+	end
+
+	local texture = checkFor(BOOKTYPE_PET)
+	if not texture then
+		texture = checkFor(BOOKTYPE_SPELL)
+	end
+
+	return texture
 end
 
 SuperMate.IsActived = function(spellName)
 	local texture = SuperMate.GetSpellTextureByName(spellName)
 	if texture ~= nil then
-		for i=1,120 do 
-			if IsCurrentAction(i) and GetActionTexture(i) == texture then 
-				return true 
-			end 
-		end 
+		for i = 1, 120 do
+			if IsCurrentAction(i) and GetActionTexture(i) == texture then
+				return true
+			end
+		end
 	end
 end
 
 SuperMate.InGCD = function()
-	local gcdActionSpell = "";
+	local gcdActionSpell = ""
 
-	local _, class = UnitClass("player");
+	local _, class = UnitClass("player")
 
 	if class == "ROGUE" then
 		gcdActionSpell = "Sinister Strike"
@@ -250,9 +263,8 @@ SuperMate.InGCD = function()
 
 	local cd = SuperMate.GetSpellCooldownByName(gcdActionSpell)
 	if cd == nil then
-		DEFAULT_CHAT_FRAME:AddMessage("Need learn "..gcdActionSpell, 1, 0, 0);
+		DEFAULT_CHAT_FRAME:AddMessage("Need learn " .. gcdActionSpell, 1, 0, 0)
 		return nil
 	end
 	return cd > 0
-
 end
